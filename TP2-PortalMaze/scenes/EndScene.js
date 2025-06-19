bcc04a1c25d0cd6500ba76351c48bf98b6806a2b
eduardagomes12-admin <eduarda.gomes@ipvc.pt>
2025-06-19 12:24:37 +0100

    endscene ajustada

--- TP2-PortalMaze/scenes/EndScene.js
+++ TP2-PortalMaze/scenes/EndScene.js
@@ -4,10 +4,17 @@ export default class EndScene extends Phaser.Scene {
     }
 
     preload() {
+        this.load.image('victory_bg', 'assets/images/menu_bg.png');
+        this.load.image('ok_button', 'assets/buttons/ok_button.png');
+        this.load.image('victory_title', 'assets/images/victory_title.png');
         this.load.audio('victorySong', 'assets/audio/Victory.mp3');
     }
 
     create() {
+
+        const centerX = this.cameras.main.centerX;
+        const centerY = this.cameras.main.centerY;
+
         // Para música da fase
         const musica = this.sound.get('musicaFundo');
         if (musica) {
@@ -19,16 +26,23 @@ export default class EndScene extends Phaser.Scene {
             this.sound.add('victorySong', { loop: true, volume: 0.6 }).play();
         }
 
-        this.add.text(300, 250, 'Parabéns!', { fontSize: '32px', fill: '#ffffff' });
-        this.add.text(280, 300, 'Pressione R para Recomeçar', { fontSize: '20px', fill: '#ffffff' });
+        // Fundo do menu
+        this.add.image(centerX, centerY, 'victory_bg')
+        .setDisplaySize(this.cameras.main.width, this.cameras.main.height)
+        .setOrigin(0.5);
 
-        this.input.keyboard.once('keydown-R', () => {
-            const victory = this.sound.get('victorySong');
-            if (victory) {
-                victory.stop();
-            }
+        // Imagem central de "Victory" com margem maior
+        this.add.image(centerX, centerY - 150, 'victory_title')
+        .setOrigin(0.5)
+        .setScale(1);
 
-            this.scene.start('MenuScene');
-        });
+        // Botão: OK → volta para o menu principal
+        const ok = this.add.image(centerX, centerY + 120, 'ok_button')
+        .setInteractive()
+        .setScale(0.6);
+
+        ok.on('pointerover', () => ok.setScale(0.65));
+        ok.on('pointerout', () => ok.setScale(0.6));
+        ok.on('pointerdown', () => this.scene.start('MenuScene'));
     }
 }
